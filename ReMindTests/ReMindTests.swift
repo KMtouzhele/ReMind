201a9a118ad1ce7ab2515dc58d050b6b10a303a0
KMtouzhele <135633869+KMtouzhele@users.noreply.github.com>
2024-11-29 15:15:55 +0800

    Initial Commit

---
+++ ReMindTests/ReMindTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReMindTests.swift
+//  ReMindTests
+//
+//  Created by Kilmer Li on 2024/11/29.
+//
+
+import Testing
+@testable import ReMind
+
+struct ReMindTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
